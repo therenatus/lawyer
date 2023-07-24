42c02c12181eb533b041aa5497c1eb092d03c625
therenatus <therinatus@gmail.com>
2023-07-24 15:41:30 +0600

    fix main.ts

--- law-backend/src/main.ts
+++ law-backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors();
-  app.use(urlencoded({ extended: true }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   app.use(json({ limit: '50mb' }));
   const config = new DocumentBuilder()
     .setTitle('The App for lawyers')
